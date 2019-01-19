c7259920238469b1dae606073eef56fc3f8f1f61
jwprillaman <jwprillaman@gmail.com>
2019-01-19 16:23:39 -0700

    added base headline grabber. tested with cnn so far

--- headlines.go
+++ headlines.go
@@ -2,25 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"golang.org/x/net/html"
-	"strings"
 	"github.com/tebeka/selenium"
 	"os"
 )
 
-//Attempt to use https://sourcegraph.com/github.com/sourcegraph/go-webkit2@master/-/blob/README.md
-
 //var sources = [...]string{"http://www.cnn.com", "http://www.foxnews.com"}
 var sources = [...]string{"http://www.cnn.com"}
 var userAgentHeader = "Mozilla/5.0 (X11; Linux x86_64; rv:64.0) Gecko/l20100101 Firefox/64.0"
+var headlineClassNames = [...]string{"cd__headline-text"}
+var headlineElementNames = [...]string{"span"}
 
 func askSources() {
 
 	const (
-		seleniumPath = "vendor/selenium-server-standalone-3.14.0.jar"
+		seleniumPath   = "vendor/selenium-server-standalone-3.14.0.jar"
 		gekoDriverPath = "vendor/geckodriver-v0.23.0-linux64"
-		port = 8080
+		port           = 8080
 	)
 
 	opts := []selenium.ServiceOption{
@@ -35,67 +32,44 @@ func askSources() {
 	}
 	defer service.Stop()
 
-
-	capabilities := selenium.Capabilities{"browserName":"firefox"}
+	capabilities := selenium.Capabilities{"browserName": "firefox"}
 	for _, source := range sources {
 
+		//TODO handle selenium warnings
 		wd, err := selenium.NewRemote(capabilities, fmt.Sprintf("http://localhost:%d/wd/hub", port))
+		fmt.Printf("%v\n", wd)
+
+		err = wd.Get(source)
 		if err != nil {
 			panic(err)
-		} else {
-			err := wd.Get(source)
-			if err != nil {
-				panic(err)
-			}
 		}
-
-
-		//old
-
+		headlines := getHeadlines(wd)
+		for _, headline := range headlines {
+			fmt.Printf("%v\n", headline)
+		}
 	}
 }
 
-func getHeadlines(reader io.Reader) []string {
+func getHeadlines(wd selenium.WebDriver) []string {
 	headlines := make([]string, 0)
-	tokenizer := html.NewTokenizer(reader)
-	isSearching := false
-	//currentSearchType := ""
-	for {
-		tokenType := tokenizer.Next()
-		if tokenType != html.ErrorToken {
-			token := tokenizer.Token()
-
-			/*fmt.Printf("%v\n", token.String())
-			fmt.Printf("\t%v\n", token.Data)
-			fmt.Printf("\t\t%v\n", string(tokenizer.Text()))
-			*/
-			if !isSearching {
-				if string(token.Data) == "span" {
-							for _, attr := range token.Attr {
-								fmt.Printf("%v = %v\n", attr.Key, attr.Val)
-							}
-				}
-
-
-				for _, attr := range token.Attr {
-					if strings.Contains(attr.Val, "headline-text") {
-						fmt.Printf("Has headline-text")
-						//currentSearchType = token.String()
-						isSearching = true
-						break
+	for _, className := range headlineClassNames {
+		elements, _ := wd.FindElements(selenium.ByCSSSelector, fmt.Sprintf(".%v", className))
+		for _, e := range elements {
+			currentText, _ := e.Text()
+			if len(currentText) > 0 {
+				headlines = append(headlines, currentText)
+			} else { //now find acceptable child
+				children, _ := e.FindElements(selenium.ByTagName, "*")
+				if len(children) > 0 {
+					for _, child := range children {
+						childText, _ := child.Text()
+						if len(childText) > 0 {
+							headlines = append(headlines, childText)
+						}
 					}
 				}
-			} else {
-				if token.String() != "strong" {
-					headlines = append(headlines, string(tokenizer.Text()))
-					isSearching = false
-				}
 			}
-		} else {
-			fmt.Printf("Token was error. Breaking....")
-			break
 		}
-
 	}
 
 	return headlines
